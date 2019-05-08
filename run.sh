298275387dc158b20e6da5fafbf3ed3651c27a65
Mirco Zeiss <mirco.zeiss@gmail.com>
2019-05-08 14:40:00 +0200

    inc value to 10.5

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ curl \
 	--verbose \
 	--header "Authorization: Token ${TOKEN}" \
 	--header "Content-Type: application/json" \
-	--data "{\"value\":7,\"sha\":\"${CIRCLE_SHA1}\"}" \
+	--data "{\"value\":10.5,\"sha\":\"${CIRCLE_SHA1}\"}" \
 	"https://seriesci.com/api/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/deps/values"
 
 exit 0
